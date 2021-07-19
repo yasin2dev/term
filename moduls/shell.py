5aad1e1c6622fc8533762c6bcd0b2da3c45401ee
yasin2dev <yasin.bozkurt.n@gmail.com>
2021-07-19 17:45:17 +0300

    Fix: moduls/shell.py

--- moduls/shell.py
+++ moduls/shell.py
@@ -1,22 +1,25 @@
 import os
 from printwith import *
 
-while (True):
+def runShell():
+    while (True):
 
-    cdPrefix = "cd"
-    currentDir = os.getcwd()
-    cmd = input(f'\33[94m{currentDir}:>\33[0m'.format(currentDir))
+        cdPrefix = "cd"
+        currentDir = os.getcwd()
+        cmd = input(f'\33[94m{currentDir}:>\33[0m'.format(currentDir))
 
-    if cmd == "restart":
-        os.system("python main.py")
-        print("Restarting...")
-        exit()
+        if cmd == "restart":
+            os.system("python main.py")
+            print("Restarting...")
+            exit()
         
-    elif cmd.startswith(cdPrefix):
-        try:
-            ncmd = cmd.split()
-            os.chdir(ncmd[1])
-        except FileNotFoundError:
-            printWith(Colors.RED, Colors.BOLD, "Sorry! I can't find your directory!")
-    else:
-        os.system(cmd)
+        elif cmd.startswith(cdPrefix):
+            try:
+                ncmd = cmd.split()
+                os.chdir(ncmd[1])
+            except FileNotFoundError:
+                printWith(Colors.RED, Colors.BOLD, "Sorry! I can't find your directory!")
+        elif cmd == "exit":
+            exit()
+        else:
+            os.system(cmd)
