d7cb057e6f16a9a1e69d40387c571ab33ba90900
yasin2dev <yasin.bozkurt.n@gmail.com>
2021-07-26 21:48:44 +0300

    Update: Architecture

--- getPc.py
+++ getPc.py
@@ -2,17 +2,23 @@ import platform, psutil, json
 from printwith import *
 
 
+def is_64bit():
+    return platform.machine().endswith('64')
+
+
 def getPc():
     info={}
     info['platform']=platform.system()
     info['platform-release']=platform.release()
     info['platform-version']=platform.version()
-    info['architecture']=platform.architecture()
     info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+ " GB"
     x = json.dumps(info)
     p = json.loads(x)
     printWithBold(Colors.GREEN, Colors.BOLD, "OS: " + p['platform'] + " " + p['platform-release'])
     printWithBold(Colors.GREEN, Colors.BOLD, "Version: " + p['platform-version'])
-    printWithBold(Colors.GREEN, Colors.BOLD, "Architecture: " + str(p['architecture']))
+    if is_64bit():
+        printWithBold(Colors.GREEN, Colors.BOLD, "Architecture: 64 bit")
+    else:
+        printWithBold(Colors.GREEN, Colors.BOLD, "Architecture: 32 bit")
     printWithBold(Colors.GREEN, Colors.BOLD, "RAM: " + p['ram'])
     print("\n")
