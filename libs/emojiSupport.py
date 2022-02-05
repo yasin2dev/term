b412f776102d1de8441723cac0e441e9b300f745
yasin2dev <yasin.bozkurt.n@gmail.com>
2022-02-05 22:58:41 +0300

    update emojis

--- libs/emojiSupport.py
+++ libs/emojiSupport.py
@@ -1,6 +1,10 @@
 #Emoji support beta-1
 
 class Emojis:
-    GRINNING_FACE = "\U0001f600"
-    SLIGHTLY_SMILING_FACE = "\U0001f642"
-    #TODO More emojis will be added.
+    grinning_face = "\U0001F600"
+    smiling_with_eyes = "\U0001F604"
+    squinting_face = "\U0001F606"
+    slightly_smiling = "\U0001F642"
+    smiling_with_hearts = "\U0001F970"
+    smiling_with_heart_eyes = "\U0001F60D"
+    #TODO Please pull request for add emoji or easily way.
