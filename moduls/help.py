7012cff0e5a2ba9d75cf988f12fdc2b70917b575
furyex2dev <yasin.bozkurt.n@gmail.com>
2023-10-18 12:19:48 +0300

    update help menu

--- moduls/help.py
+++ moduls/help.py
@@ -2,7 +2,7 @@ from libs.printwith import *
 from libs.emoji import *
 
 def HelpText():
-    printWithRegular(Colors.YELLOW, (f"""
+    printWithBold(Colors.YELLOW, Colors.BOLD, (f"""
         Here is the Term Help!
 
         cd          Change Directory (default)
@@ -15,6 +15,14 @@ def HelpText():
         config      Configurate Term.
 
     """))
+
+    printWithBold(Colors.GREEN, Colors.BOLD, """
+        help [topic]
+        
+        emojis      Print all emojis
+    
+    """)
+
     printWithRegular(Colors.YELLOW, "Colors: ")
     printWithRegular(Colors.BLUE, "BLUE")
     printWithRegular(Colors.GREEN, "GREEN")
